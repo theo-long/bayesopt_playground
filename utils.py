861006bb7a80bd9c426faf781d4a6e3a2810cf71
Theo Long <theodorelong98@gmail.com>
2023-01-17 12:43:58 +0000

    add parallelization

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 from .optimization_loops import optimization_loop, get_recommendation
 from .benchmarks import generate_optimization_task
 
+from functools import partial
+
+from botorch.models.cost import AffineFidelityCostModel
+
 import pandas as pd
 import os
+import multiprocessing
 
 
 def run_experiment(
@@ -14,12 +19,13 @@ def run_experiment(
     multi_fidelity,
     num_runs=20,
     num_iters=50,
-    initial_samples=10,
+    initial_samples=5,
+    parallelized=True,
 ):
     """Run a single experiment on multiple benchmarks."""
-    if not os.path.exists('./data'):
-        os.mkdir('./data')
-        
+    if not os.path.exists("./data"):
+        os.mkdir("./data")
+
     all_results = []
     for benchmark in benchmarks:
         print(f"Starting {benchmark.__name__}")
@@ -27,14 +33,10 @@ def run_experiment(
 
         log_transform_indices = getattr(benchmark, "log_transform_indices", None)
         if log_transform_indices:
-            old_mf = model_factory
-            old_cmf = cost_model_factory
-            model_factory = lambda *args: old_mf(
-                *args, log_transform_indices=log_transform_indices
-            )
-            cost_model_factory = lambda *args: old_cmf(
-                *args, log_transform_indices=log_transform_indices
-            )
+            model_factory = partial(model_factory, log_transform_indices=log_transform_indices)
+
+            if not isinstance(cost_model_factory, AffineFidelityCostModel):
+                cost_model_factory = partial(cost_model_factory, log_transform_indices=log_transform_indices)
 
         pass_current_best = getattr(acquisition_factory, "pass_current_best", False)
         full_fidelity = not multi_fidelity
@@ -44,38 +46,33 @@ def run_experiment(
         else:
             fidelity_samples = None
 
-        for run in range(num_runs):
-            print(f"Run {run+1}/{num_runs}", end="\r")
+        def args_iterable():
+            for run in range(num_runs):
+                yield (
+                    run,
+                    model_factory,
+                    cost_model_factory,
+                    acquisition_factory,
+                    objective_function,
+                    bounds,
+                    num_iters,
+                    initial_samples,
+                    fidelity_samples,
+                    log_transform_indices,
+                    full_fidelity,
+                    pass_current_best,
+                    benchmark,
+                )
 
-            model, train_x, train_obj, full_results = optimization_loop(
-                model_factory,
-                cost_model_factory,
-                acquisition_factory,
-                objective_function,
-                bounds,
-                n_iter=num_iters,
-                initial_samples=initial_samples,
-                fidelity_samples=fidelity_samples,
-                log_transform_indices=log_transform_indices,
-                full_fidelity=full_fidelity,
-                pass_current_best=pass_current_best,
-            )
-            final_rec, objective_value, results = get_recommendation(
-                model, objective_function, bounds, full_fidelity=full_fidelity
-            )
-            full_results += results
-            evaluations_df = pd.concat(
-                [
-                    pd.DataFrame(train_x.detach().cpu().numpy()),
-                    pd.DataFrame(final_rec.detach().cpu().numpy()),
-                ]
-            ).reset_index(drop=True)
-            results_df = pd.DataFrame(full_results)
-            results_df["run"] = run
-            results_df["benchmark"] = benchmark.__name__
-            results_df = results_df.reset_index()
-            # results_df = pd.concat([results_df, evaluations_df], axis=1)
-            all_results.append(results_df)
+        if parallelized:
+            with multiprocessing.Pool(multiprocessing.cpu_count) as pool:
+                results = pool.starmap(single_run, args_iterable())
+        else:
+            results = []
+            for args in args_iterable:
+                results.append(single_run(*args))
+        
+        all_results += results
 
         # Save after every benchmark in case of partial run
         all_results_df = pd.concat(all_results)
@@ -84,4 +81,50 @@ def run_experiment(
     all_results_df = pd.concat(all_results)
     all_results_df.to_csv(f"./data/{experiment_name}.csv")
 
-    return evaluations_df, results_df
+
+def single_run(
+    run,
+    model_factory,
+    cost_model_factory,
+    acquisition_factory,
+    objective_function,
+    bounds,
+    num_iters,
+    initial_samples,
+    fidelity_samples,
+    log_transform_indices,
+    full_fidelity,
+    pass_current_best,
+    benchmark,
+):
+    print(f"Run {run+1}", end="\r")
+
+    model, train_x, train_obj, full_results = optimization_loop(
+        model_factory,
+        cost_model_factory,
+        acquisition_factory,
+        objective_function,
+        bounds,
+        n_iter=num_iters,
+        initial_samples=initial_samples,
+        fidelity_samples=fidelity_samples,
+        log_transform_indices=log_transform_indices,
+        full_fidelity=full_fidelity,
+        pass_current_best=pass_current_best,
+    )
+    final_rec, objective_value, results = get_recommendation(
+        model, objective_function, bounds, full_fidelity=full_fidelity
+    )
+    full_results += results
+    evaluations_df = pd.concat(
+        [
+            pd.DataFrame(train_x.detach().cpu().numpy()),
+            pd.DataFrame(final_rec.detach().cpu().numpy()),
+        ]
+    ).reset_index(drop=True)
+    results_df = pd.DataFrame(full_results)
+    results_df["run"] = run
+    results_df["benchmark"] = benchmark.__name__
+    results_df = results_df.reset_index()
+    results_df = pd.concat([results_df, evaluations_df], axis=1)
+    return results_df
